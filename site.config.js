262091b0b3393eaf4f3224ff9854f16be21603c5
Mingqi Song <84456587+angel-song-1997@users.noreply.github.com>
2021-09-14 20:00:15 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'fb4b8008ff304c73a5d282ce156304e0',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Mingqi(Angel) Song',
+  domain: 'https://mirror-rose-787.notion.site/Mingqi-Angel-Song-fb4b8008ff304c73a5d282ce156304e0',
+  author: 'Mingqi Song',
 
   // open graph metadata (optional)
   description: 'Example site description',
@@ -17,9 +17,7 @@ module.exports = {
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  linkedin: 'Mingqi Song',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
